d1741747bfd1d72b6f9a20771556e538aa4a0ce4
Ekolom <82455383+Ekolom@users.noreply.github.com>
2021-09-19 00:34:08 -0500

    Add files via upload

---
+++ Exercise 4/Ex04.01/Ex04.01/reverseString.h
@@ -0,0 +1,19 @@
+/**
+ * @file reverseString.h
+ * @author Elazar Kolom
+ * @brief This file contains the decleration to the reverseString function
+ * @date 9/15/21
+ */
+
+#ifndef reverseString_h
+#define reverseString_h
+
+#include <stdio.h>
+
+/**
+ * @brief Function prints the reverse order of the characters in a string
+ * @param str The string to be reversed (array of characters)
+ */
+void reverseString(char *str);
+
+#endif /* reverseString_h */
